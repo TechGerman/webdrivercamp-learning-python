dcda2765a07415ae3139bcd6be21e664a16b0b29
TechGerman <ter.germanv@gmail.com>
2024-09-29 20:45:59 -0700

    Changes in file 3_printprint.py

--- python_beginning/3_printprint.py
+++ python_beginning/3_printprint.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 confirmation_message = "Yes!"
-print(confirmation_message)
-print(confirmation_message)
-print(confirmation_message)
-print(confirmation_message)
+print(confirmation_message * 4)
